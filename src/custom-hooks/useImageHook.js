5c3a6137a8856bc139e7bddeb855aa478deb4754
sanjeevan <s.y.gunasekaran@accenture.com>
2022-12-20 13:20:14 +0530

    error handling

--- src/custom-hooks/useImageHook.js
+++ src/custom-hooks/useImageHook.js
@@ -13,23 +13,25 @@ export default function useLoadImageHook(query) {
   //const apiKey = "636e1481b4f3c446d26b8eb6ebfe7127";
   useEffect(() => {
     setIsLoading(true)
+    setError(false)
     const controller = new AbortController();
     axios.get(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=20&format=json&nojsoncallback=1`,
       {
         signal: controller.signal
       })
       .then(response => {
-
         setImages(response.data.photos.photo);
         setIsLoading(false);
       })
       .catch(error => {
-        console.log(
-          "Encountered an error with fetching and parsing data",
-          error
-        )
-        setIsLoading(false);
-        setError(true);
+        if (error.name !== 'CanceledError') {
+          console.log(
+            "Encountered an error with fetching and parsing data",
+            error
+          )
+          setIsLoading(false);
+          setError(true);
+        }
       });
 
     return () => {
--- src/index.js
+++ src/index.js
@@ -7,9 +7,12 @@ import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <HashRouter>
-    <App />
-  </HashRouter>
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
